33742c893cf74f38ea7d95323df6aa67fdc7ce1e
SugmaNot <seraphim.m.chistov@gmail.com>
2026-08-06 19:51:12 +0300

    feat: export Challenge Capsule validators

--- src/validation.ts
+++ src/validation.ts
@@ -1,3 +1,10 @@
 export { validateId, validateResourceLocation } from "./validation/common.js";
 export { validateManifest, validateDescriptor, descriptorIntegrity, validateRegistry } from "./validation/package.js";
 export { validateProfile, validateLockfile, validateJournal } from "./validation/state.js";
+export {
+  validateChallengeCapsule,
+  validateChallengeSession,
+  validateChallengeTicket,
+  validateChallengeResult,
+  validateClaimsRecord
+} from "./validation/challenge.js";
